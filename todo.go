b7d06032df8d4548ee692426e23dc31871f601e9
pankona <yosuke.akatsuka@gmail.com>
2017-01-31 20:03:21 +0900

    generate new ToDo

--- todo.go
+++ todo.go
@@ -1,10 +1,23 @@
 package todo
 
+import (
+	"errors"
+	"log"
+)
+
 // ToDo represents a struct of ToDo item
 type ToDo struct {
+	desc string
 }
 
 // NewToDo allocates a ToDo item
-func NewToDo() *ToDo {
-	return &ToDo{}
+func NewToDo(desc string) *ToDo {
+	return &ToDo{desc: desc}
+}
+
+// Add adds a specified ToDo item to DB
+func Add(t *ToDo) error {
+	log.Println("Add. t =", t.desc)
+	// TODO: store desc to DB
+	return errors.New("Add failed")
 }
