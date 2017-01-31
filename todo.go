64119c9f5b3cc2d49e5619b6a85230bb5255090b
pankona <yosuke.akatsuka@gmail.com>
2017-01-31 20:20:19 +0900

    add todo to db

--- todo.go
+++ todo.go
@@ -1,8 +1,13 @@
 package todo
 
 import (
+	"database/sql"
 	"errors"
 	"log"
+	"os"
+
+	// go-sqlite3 only imported here
+	_ "github.com/mattn/go-sqlite3"
 )
 
 // ToDo represents a struct of ToDo item
@@ -19,5 +24,56 @@ func NewToDo(desc string) *ToDo {
 func Add(t *ToDo) error {
 	log.Println("Add. t =", t.desc)
 	// TODO: store desc to DB
-	return errors.New("Add failed")
+
+	// TODO: support multi platform
+	homeDir := os.Getenv("HOME")
+
+	// TODO: error check
+	_, err := os.Create(homeDir + "/.todo.db")
+	if err != nil {
+		return errors.New("failed to create db file")
+	}
+
+	db, err := sql.Open("sqlite3", homeDir+"/.todo.db")
+	if err != nil {
+		return errors.New("failed to open database")
+	}
+
+	err = execCreateTable(db)
+	if err != nil {
+		return errors.New("failed to create table")
+	}
+
+	err = execAddToDo(db, t)
+	if err != nil {
+		return errors.New("failed to add ToDo")
+	}
+
+	return nil
+}
+
+// TODO: this should be done at initialization
+func execCreateTable(db *sql.DB) error {
+	q := "CREATE TABLE todo ("
+	q += " id INTEGER PRIMARY KEY AUTOINCREMENT"
+	q += ", desc VARCHAR(255) NOT NULL"
+	q += ", created_at TIMESTAMP DEFAULT (DATETIME('now','localtime'))"
+	q += ")"
+	_, err := db.Exec(q)
+	if err != nil {
+		return errors.New("failed to create table")
+	}
+	return nil
+}
+
+func execAddToDo(db *sql.DB, t *ToDo) error {
+	q := "INSERT INTO todo "
+	q += " (desc)"
+	q += " VALUES"
+	q += " ('" + t.desc + "')"
+	_, err := db.Exec(q)
+	if err != nil {
+		return errors.New("failed to create table")
+	}
+	return nil
 }
