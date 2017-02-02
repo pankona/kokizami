07e630ddaa5f6ffa66da3e84987e5f7a2923cfe1
pankona <yosuke.akatsuka@gmail.com>
2017-02-03 02:22:01 +0900

    refactoring. CmdFunctions combined into one file

--- cmd/todo/commands.go
+++ cmd/todo/commands.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"strconv"
 
 	"github.com/codegangsta/cli"
+	"github.com/pankona/todo"
 )
 
 // GlobalFlags can be used globally
@@ -42,4 +45,73 @@ var Commands = []cli.Command{
 func CommandNotFound(c *cli.Context, command string) {
 	fmt.Fprintf(os.Stderr, "%s: '%s' is not a %s command. See '%s --help'.", c.App.Name, command, c.App.Name, c.App.Name)
 	os.Exit(2)
+}
+
+// CmdAdd adds a new todo
+func CmdAdd(c *cli.Context) {
+	for i, v := range c.Args() {
+		switch i {
+		case 0:
+			t, err := todo.Add(v)
+			if err != nil {
+				log.Println(err)
+			}
+			log.Println(t)
+		}
+	}
+}
+
+// CmdEdit adds a new todo
+func CmdEdit(c *cli.Context) {
+	args := c.Args()
+	if len(args) != 2 {
+		log.Println("edit needs two arguments (id, desc)")
+		return
+	}
+
+	id, err := strconv.Atoi(args[0])
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	desc := args[1]
+	t, err := todo.Edit(id, desc)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println(t)
+}
+
+// CmdList shows ToDo list
+func CmdList(c *cli.Context) {
+	l, err := todo.List()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	for _, v := range l {
+		if v.IsDone() {
+			continue
+		}
+		log.Println(v)
+	}
+}
+
+// CmdDone marks specified ToDo as done
+func CmdDone(c *cli.Context) {
+	for i, v := range c.Args() {
+		switch i {
+		case 0:
+			id, err := strconv.Atoi(v)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			err = todo.Done(id)
+			if err != nil {
+				log.Println(err)
+			}
+		}
+	}
 }
