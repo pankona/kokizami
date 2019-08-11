ec22ceb71c5592711499aff76215d8737c9237f5
pankona <yosuke.akatsuka@gmail.com>
2019-08-11 09:20:50 +0900

    open DB connection at kokizami's initialize

--- kokizami.go
+++ kokizami.go
@@ -14,7 +14,7 @@ import (
 // Kokizami provides most APIs of kokizami library
 type Kokizami struct {
 	DBPath string
-	conn   *sql.DB
+	db     *sql.DB
 	now    func() time.Time
 }
 
@@ -25,19 +25,6 @@ func initialTime() time.Time {
 		panic(fmt.Sprintf("failed to parse time for initial value for time: %v", err))
 	}
 	return t.UTC()
-}
-
-// Deprecated: DB connection should be established in Initialize function
-func (k *Kokizami) execWithDB(f func(db *sql.DB) error) error {
-	if k.conn == nil {
-		conn, err := sql.Open("sqlite3", k.DBPath)
-		if err != nil {
-			return err
-		}
-		k.conn = conn
-	}
-
-	return f(k.conn)
 }
 
 // EnableVerboseQuery toggles debug logging by argument
@@ -51,33 +38,39 @@ func (k *Kokizami) EnableVerboseQuery(enable bool) {
 
 // Initialize initializes Kokizami
 // Kokizami's member field must be fulfilled in advance of calling this function
-// TODO: Establish DB connection here and retain it in receiver until Finalize
 func (k *Kokizami) Initialize() error {
 	if k.now == nil {
 		k.now = time.Now
 	}
 
-	return k.execWithDB(func(db *sql.DB) error {
-		if err := models.CreateKizamiTable(db); err != nil {
-			return fmt.Errorf("failed to create kizami table: %v", err)
+	if k.db == nil {
+		db, err := sql.Open("sqlite3", k.DBPath)
+		if err != nil {
+			return err
 		}
-		if err := models.CreateTagTable(db); err != nil {
-			return fmt.Errorf("failed to create tag table: %v", err)
-		}
-		if err := models.CreateRelationTable(db); err != nil {
-			return fmt.Errorf("failed to create relation table: %v", err)
-		}
-		return nil
-	})
+		k.db = db
+	}
+
+	if err := models.CreateKizamiTable(k.db); err != nil {
+		return fmt.Errorf("failed to create kizami table: %v", err)
+	}
+	if err := models.CreateTagTable(k.db); err != nil {
+		return fmt.Errorf("failed to create tag table: %v", err)
+	}
+	if err := models.CreateRelationTable(k.db); err != nil {
+		return fmt.Errorf("failed to create relation table: %v", err)
+	}
+
+	return nil
 }
 
 // Finalize finalizes kokizami
 func (k *Kokizami) Finalize() error {
-	if k.conn == nil {
+	if k.db == nil {
 		return nil
 	}
 
-	return k.conn.Close()
+	return k.db.Close()
 }
 
 // Start starts a new kizami with specified desc
@@ -86,273 +79,233 @@ func (k *Kokizami) Start(desc string) (*Kizami, error) {
 		return nil, fmt.Errorf("desc must not be empty")
 	}
 
-	var ki *Kizami
-	return ki, k.execWithDB(func(db *sql.DB) error {
-		entry := &models.Kizami{
-			Desc:      desc,
-			StartedAt: models.SqTime(k.now().UTC()),
-			StoppedAt: models.SqTime(initialTime()),
-		}
-		err := entry.Insert(db)
-		if err != nil {
-			return err
-		}
+	entry := &models.Kizami{
+		Desc:      desc,
+		StartedAt: models.SqTime(k.now().UTC()),
+		StoppedAt: models.SqTime(initialTime()),
+	}
+	err := entry.Insert(k.db)
+	if err != nil {
+		return nil, err
+	}
 
-		m, err := models.KizamiByID(db, entry.ID)
-		ki = toKizami(m)
-		return err
-	})
+	m, err := models.KizamiByID(k.db, entry.ID)
+	return toKizami(m), err
 }
 
 // Get returns a Kizami by specified ID
 func (k *Kokizami) Get(id int) (*Kizami, error) {
-	var ki *Kizami
-	return ki, k.execWithDB(func(db *sql.DB) error {
-		m, err := models.KizamiByID(db, id)
-		if err != nil {
-			return err
-		}
-		ki = toKizami(m)
-		return nil
-	})
+	m, err := models.KizamiByID(k.db, id)
+	if err != nil {
+		return nil, err
+	}
+	return toKizami(m), nil
 }
 
 // Edit edits a specified kizami and update its model
 func (k *Kokizami) Edit(ki *Kizami) (*Kizami, error) {
-	return ki, k.execWithDB(func(db *sql.DB) error {
-		m, err := models.KizamiByID(db, ki.ID)
-		if err != nil {
-			return err
-		}
+	m, err := models.KizamiByID(k.db, ki.ID)
+	if err != nil {
+		return nil, err
+	}
 
-		m.Desc = ki.Desc
-		m.StartedAt = models.SqTime(ki.StartedAt.UTC())
-		m.StoppedAt = models.SqTime(ki.StoppedAt.UTC())
+	m.Desc = ki.Desc
+	m.StartedAt = models.SqTime(ki.StartedAt.UTC())
+	m.StoppedAt = models.SqTime(ki.StoppedAt.UTC())
 
-		err = m.Update(db)
-		if err != nil {
-			return err
-		}
-		m, err = models.KizamiByID(db, ki.ID)
-		ki = toKizami(m)
-		return err
-	})
+	err = m.Update(k.db)
+	if err != nil {
+		return nil, err
+	}
+	m, err = models.KizamiByID(k.db, ki.ID)
+	return toKizami(m), err
 }
 
 // Stop stops a on-going kizami by specified ID
 func (k *Kokizami) Stop(id int) error {
-	return k.execWithDB(func(db *sql.DB) error {
-		ki, err := models.KizamiByID(db, id)
-		if err != nil {
-			return err
-		}
-		ki.StoppedAt = models.SqTime(k.now().UTC())
-		return ki.Update(db)
-	})
+	ki, err := models.KizamiByID(k.db, id)
+	if err != nil {
+		return err
+	}
+	ki.StoppedAt = models.SqTime(k.now().UTC())
+	return ki.Update(k.db)
 }
 
 // StopAll stops all on-going kizamis
 func (k *Kokizami) StopAll() error {
-	return k.execWithDB(func(db *sql.DB) error {
-		ks, err := models.KizamisByStoppedAt(db, models.SqTime(initialTime()))
-		if err != nil {
+	ks, err := models.KizamisByStoppedAt(k.db, models.SqTime(initialTime()))
+	if err != nil {
+		return err
+	}
+	now := k.now().UTC()
+	for i := range ks {
+		ks[i].StoppedAt = models.SqTime(now)
+		if err := ks[i].Update(k.db); err != nil {
 			return err
 		}
-		now := k.now().UTC()
-		for i := range ks {
-			ks[i].StoppedAt = models.SqTime(now)
-			if err := ks[i].Update(db); err != nil {
-				return err
-			}
-		}
-		return nil
-	})
+	}
+	return nil
 }
 
 // Delete deletes a kizami by specified ID
 func (k *Kokizami) Delete(id int) error {
-	return k.execWithDB(func(db *sql.DB) error {
-		ki, err := models.KizamiByID(db, id)
-		if err != nil {
-			return err
-		}
-		return ki.Delete(db)
-	})
+	ki, err := models.KizamiByID(k.db, id)
+	if err != nil {
+		return err
+	}
+	return ki.Delete(k.db)
 }
 
 // List returns all Kizamis
 func (k *Kokizami) List() ([]Kizami, error) {
-	var ks []Kizami
-	return ks, k.execWithDB(func(db *sql.DB) error {
-		ms, err := models.AllKizami(db)
-		if err != nil {
-			return err
-		}
+	ms, err := models.AllKizami(k.db)
+	if err != nil {
+		return nil, err
+	}
 
-		ks = make([]Kizami, len(ms))
-		for i := range ms {
-			ks[i].ID = ms[i].ID
-			ks[i].Desc = ms[i].Desc
-			ks[i].StartedAt = ms[i].StartedAt.Time
-			ks[i].StoppedAt = ms[i].StoppedAt.Time
-		}
-		return nil
-	})
+	ks := make([]Kizami, len(ms))
+	for i := range ms {
+		ks[i].ID = ms[i].ID
+		ks[i].Desc = ms[i].Desc
+		ks[i].StartedAt = ms[i].StartedAt.Time
+		ks[i].StoppedAt = ms[i].StoppedAt.Time
+	}
+
+	return ks, nil
 }
 
 // SummaryByTag returns total elapsed time of Kizamis in specified month grouped by tag
 func (k *Kokizami) SummaryByTag(yyyymm string) ([]Elapsed, error) {
-	var s []Elapsed
-
 	// validate input
 	_, err := time.Parse("2006-01", yyyymm)
 	if err != nil {
 		return nil, fmt.Errorf("invalid argument format. should be yyyy-mm: %v", err)
 	}
 
-	return s, k.execWithDB(func(db *sql.DB) error {
-		ms, err := models.ElapsedOfMonthByTag(db, yyyymm)
-		if err != nil {
-			return err
-		}
+	ms, err := models.ElapsedOfMonthByTag(k.db, yyyymm)
+	if err != nil {
+		return nil, err
+	}
 
-		s = make([]Elapsed, len(ms))
-		for i := range ms {
-			s[i].Tag = ms[i].Tag
-			s[i].Desc = ms[i].Desc
-			s[i].Count = ms[i].Count
-			s[i].Elapsed = ms[i].Elapsed
-		}
-		return nil
-	})
+	s := make([]Elapsed, len(ms))
+	for i := range ms {
+		s[i].Tag = ms[i].Tag
+		s[i].Desc = ms[i].Desc
+		s[i].Count = ms[i].Count
+		s[i].Elapsed = ms[i].Elapsed
+	}
+
+	return s, nil
 }
 
 // SummaryByDesc returns total elapsed time of Kizamis in specified month grouped by desc
 func (k *Kokizami) SummaryByDesc(yyyymm string) ([]Elapsed, error) {
-	var s []Elapsed
-
 	// validate input
 	_, err := time.Parse("2006-01", yyyymm)
 	if err != nil {
 		return nil, fmt.Errorf("invalid argument format. should be yyyy-mm: %v", err)
 	}
 
-	return s, k.execWithDB(func(db *sql.DB) error {
-		ms, err := models.ElapsedOfMonthByDesc(db, yyyymm)
-		if err != nil {
-			return err
-		}
+	ms, err := models.ElapsedOfMonthByDesc(k.db, yyyymm)
+	if err != nil {
+		return nil, err
+	}
 
-		s = make([]Elapsed, len(ms))
-		for i := range ms {
-			s[i].Tag = ms[i].Tag
-			s[i].Desc = ms[i].Desc
-			s[i].Count = ms[i].Count
-			s[i].Elapsed = ms[i].Elapsed
-		}
-		return nil
-	})
+	s := make([]Elapsed, len(ms))
+	for i := range ms {
+		s[i].Tag = ms[i].Tag
+		s[i].Desc = ms[i].Desc
+		s[i].Count = ms[i].Count
+		s[i].Elapsed = ms[i].Elapsed
+	}
+
+	return s, nil
 }
 
 // AddTags adds a new tags
 func (k *Kokizami) AddTags(tags []string) error {
-	return k.execWithDB(func(db *sql.DB) error {
-		ts := models.Tags(make([]models.Tag, len(tags)))
-		for i := range ts {
-			// skip empty string
-			if len(tags[i]) == 0 {
-				ts = ts[:len(ts)-1]
-				continue
-			}
-			ts[i].Tag = tags[i]
+	ts := models.Tags(make([]models.Tag, len(tags)))
+
+	for i := range ts {
+		// skip empty string
+		if len(tags[i]) == 0 {
+			ts = ts[:len(ts)-1]
+			continue
 		}
-		return ts.BulkInsert(db)
-	})
+		ts[i].Tag = tags[i]
+	}
+
+	return ts.BulkInsert(k.db)
 }
 
 // DeleteTag deletes a specified tag
 func (k *Kokizami) DeleteTag(id int) error {
-	return k.execWithDB(func(db *sql.DB) error {
-		m, err := models.TagByID(db, id)
-		if err != nil {
-			return err
-		}
-		return m.Delete(db)
-	})
+	m, err := models.TagByID(k.db, id)
+	if err != nil {
+		return err
+	}
+	return m.Delete(k.db)
 }
 
 // Tags returns list of tags
 func (k *Kokizami) Tags() ([]Tag, error) {
-	var ts []Tag
-	return ts, k.execWithDB(func(db *sql.DB) error {
-		ms, err := models.AllTags(db)
-		if err != nil {
-			return err
-		}
+	ms, err := models.AllTags(k.db)
+	if err != nil {
+		return nil, err
+	}
 
-		ts = make([]Tag, len(ms))
-		for i := range ms {
-			ts[i].ID = ms[i].ID
-			ts[i].Tag = ms[i].Tag
-		}
+	ts := make([]Tag, len(ms))
+	for i := range ms {
+		ts[i].ID = ms[i].ID
+		ts[i].Tag = ms[i].Tag
+	}
 
-		return nil
-	})
+	return ts, nil
 }
 
 // Tagging makes relation between specified kizami and tags
 func (k *Kokizami) Tagging(kizamiID int, tagIDs []int) error {
-	return k.execWithDB(func(db *sql.DB) error {
-		rs := models.Relations(make([]models.Relation, len(tagIDs)))
-		for i := range rs {
-			rs[i].KizamiID = kizamiID
-			rs[i].TagID = tagIDs[i]
+	rs := models.Relations(make([]models.Relation, len(tagIDs)))
+	for i := range rs {
+		rs[i].KizamiID = kizamiID
+		rs[i].TagID = tagIDs[i]
 
-		}
-		return rs.BulkInsert(db)
-	})
+	}
+	return rs.BulkInsert(k.db)
 }
 
 // Untagging removes all tags from specified kizami
 func (k *Kokizami) Untagging(kizamiID int) error {
-	return k.execWithDB(func(db *sql.DB) error {
-		return models.DeleteRelationsByKizamiID(db, kizamiID)
-	})
+	return models.DeleteRelationsByKizamiID(k.db, kizamiID)
 }
 
 // TagsByKizamiID returns tags of specified kizami
 func (k *Kokizami) TagsByKizamiID(kizamiID int) ([]Tag, error) {
-	var ts []Tag
-	return ts, k.execWithDB(func(db *sql.DB) error {
-		ms, err := models.TagsByKizamiID(db, kizamiID)
-		if err != nil {
-			return err
-		}
+	ms, err := models.TagsByKizamiID(k.db, kizamiID)
+	if err != nil {
+		return nil, err
+	}
 
-		ts = make([]Tag, len(ms))
-		for i := range ms {
-			ts[i].ID = ms[i].ID
-			ts[i].Tag = ms[i].Tag
-		}
+	ts := make([]Tag, len(ms))
+	for i := range ms {
+		ts[i].ID = ms[i].ID
+		ts[i].Tag = ms[i].Tag
+	}
 
-		return nil
-	})
+	return ts, nil
 }
 
 // TagsByTags returns tags by specified tags
 func (k *Kokizami) TagsByTags(tags []string) ([]Tag, error) {
-	var ts []Tag
-	return ts, k.execWithDB(func(db *sql.DB) error {
-		ms, err := models.TagsByTags(db, tags)
-		if err != nil {
-			return err
-		}
+	ms, err := models.TagsByTags(k.db, tags)
+	if err != nil {
+		return nil, err
+	}
 
-		ts = make([]Tag, len(ms))
-		for i := range ms {
-			ts[i] = toTag(&ms[i])
-		}
+	ts := make([]Tag, len(ms))
+	for i := range ms {
+		ts[i] = toTag(&ms[i])
+	}
 
-		return nil
-	})
+	return ts, nil
 }
